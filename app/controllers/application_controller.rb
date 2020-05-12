3c86f7f4ac1c1f7b0e0206d62a2eef0498db6e9a
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-12 22:54:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id' do
     @article = Article.find_by(params[:id])
     @article.delete
-  redirect to '/articles'
+  redirect to '/delete'
   end
 
 
