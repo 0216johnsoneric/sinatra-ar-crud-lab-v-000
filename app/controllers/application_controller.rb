88972a30a0659ec060060e6aad6d632755e3f6e3
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-12 22:34:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,8 +28,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles/:id/edit' do
-    # @article = Article.find(params[:id])
-    # erb :edit
+    @article = Article.find(params[:id])
+    erb :edit
   end
 
   # patch '/articles/:id' do
